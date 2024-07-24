cfbcf9424cc898930483118548c9ca4bcbc63287
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-07-24 20:57:29 +0200

    New code ..

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,4 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Example logic: resolving the promise
-        resolve('Success');
-    });
+    return new Promise.resolve();
 }
