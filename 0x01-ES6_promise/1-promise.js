1e906e337bc8a42ebc7bbbbfd1b52216d82110af
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-07-24 19:55:29 +0200

    updated

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-// 1-promise.js
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  const java = {
+    status: 200,
+    body: 'Success',
+  };
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(java);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
